b5cabed5f3dac323a4929bc6adea629d77c8ba39
Sachin Shelke <48155218+hisachinshelke@users.noreply.github.com>
2023-05-28 16:32:54 +0530

    Saving multiple students in DB

--- 01-cruddemo-student/src/main/java/com/shelke/sachin/cruddemo/CruddemoApplication.java
+++ 01-cruddemo-student/src/main/java/com/shelke/sachin/cruddemo/CruddemoApplication.java
@@ -17,8 +17,28 @@ public class CrudDemoApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) { //From Sp. Boot Framework - executed after spring beans are loaded
         return runner -> {
-            createStudent(studentDAO);
+            // createStudent(studentDAO);
+
+            createMultipleStudents(studentDAO);
         };
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+
+        //create multiple students
+        System.out.println("Creating the student object>>>>");
+        Student tempStudent1 = new Student("Rahul", "Dravid", "rahul@rahul.rahul");
+        Student tempStudent2 = new Student("John", "Foo", "john@john.john");
+        Student tempStudent3 = new Student("Mary", "Fnn", "mary@mary.mary");
+        Student tempStudent4 = new Student("Jack", "Fpp", "jack@jack.jack");
+
+        //save the students
+        System.out.println("Saving students>>>>");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
+        studentDAO.save(tempStudent4);
+
     }
 
     private void createStudent(StudentDAO studentDAO) {
