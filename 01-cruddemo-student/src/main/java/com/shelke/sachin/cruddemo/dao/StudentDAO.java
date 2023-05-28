c325cfe22dfc94e1fdf0dd50f53faf45d540fd68
Sachin Shelke <48155218+hisachinshelke@users.noreply.github.com>
2023-05-28 19:23:40 +0530

    Changes for Query to get all students list

--- 01-cruddemo-student/src/main/java/com/shelke/sachin/cruddemo/dao/StudentDAO.java
+++ 01-cruddemo-student/src/main/java/com/shelke/sachin/cruddemo/dao/StudentDAO.java
@@ -2,10 +2,13 @@ package com.shelke.sachin.cruddemo.dao;
 
 import com.shelke.sachin.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO { // acts as a mediator between app and db
 
     void save(Student theStudent);
 
     Student findById(Integer id);
 
+    List<Student> findAll();
 }
--- 01-cruddemo-student/src/main/java/com/shelke/sachin/cruddemo/dao/StudentDAOImpl.java
+++ 01-cruddemo-student/src/main/java/com/shelke/sachin/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.shelke.sachin.cruddemo.dao;
 
 import com.shelke.sachin.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository //support for Component scanning and exception translation
 public class StudentDAOImpl implements StudentDAO {
@@ -28,5 +31,15 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id); //entityClass and primary key
+    }
+
+    @Override
+    public List<Student> findAll() {
+        //create query
+        //take the entity class names and not database table names
+        TypedQuery<Student> theQuery = entityManager.createQuery("From Student", Student.class);
+
+        //return query result
+        return theQuery.getResultList();
     }
 }
